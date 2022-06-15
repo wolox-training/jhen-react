567776728ee88f1f81b1c333753ed20fa57e5000
julian Henao <julianhernao.marin.contractor@gdevtools.com>
2022-06-14 21:38:20 -0500

    Update configuration of i18n and yup locales

---
+++ src/config/yup.ts
@@ -0,0 +1,5 @@
+import * as yup from 'yup';
+
+import { yupLocale } from 'utils/yupLocale';
+
+yup.setLocale(yupLocale);
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 
 import App from 'components/App';
 import 'config/i18n';
+import 'config/yup';
 import 'scss/application.scss';
 
 import reportWebVitals from './reportWebVitals';
