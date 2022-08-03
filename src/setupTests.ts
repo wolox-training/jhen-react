127acef4ea91058b3bd396acc3719ab8919752f2
julian Henao <julianhernao.marin.contractor@gdevtools.com>
2022-08-03 11:03:23 -0500

    Configure test library in the project

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,16 @@
 import '@testing-library/jest-dom';
 import 'mutationobserver-shim';
 
+import { mswServer } from './mocks/msw-server';
+
+beforeAll(() => mswServer.listen());
+afterEach(() => mswServer.resetHandlers());
+afterAll(() => mswServer.close());
+
 jest.mock('i18next', () => ({
   t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key)
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({ t: (key: any) => key })
 }));
