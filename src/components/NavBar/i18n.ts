045d3d1eeceb52e7978356d85c608be21ef82e36
Julian Henao Marin <julianhm9612@gmail.com>
2022-09-06 16:41:01 -0500

    Improve styles, i18n and html in navBar component

---
+++ src/components/NavBar/i18n.ts
@@ -0,0 +1,11 @@
+import i18next from 'i18next';
+
+i18next.addResources('es', 'Nav', {
+  logout: 'Logout',
+  logoAlt: 'Logo original de Wolox'
+});
+
+i18next.addResources('en', 'Nav', {
+  logout: 'Logout',
+  logoAlt: 'Wolox original logo'
+});
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 import { logoutSession } from 'services/UserService';
@@ -7,6 +8,7 @@ import { logoutSession } from 'services/UserService';
 import logo from 'assets/logo-wolox-original.png';
 
 function NavBar() {
+  const { t } = useTranslation();
   const navigate = useNavigate();
 
   const logout = () => {
@@ -15,14 +17,12 @@ function NavBar() {
   };
 
   return (
-    <div className={styles.navbar}>
-      <nav className={styles.nav}>
-        <img src={logo} className={styles.logo} alt="Logo original de wolox" />
-        <button className={styles.logout} onClick={logout}>
-          Logout
-        </button>
-      </nav>
-    </div>
+    <nav className={styles.nav}>
+      <img src={logo} className={styles.logo} alt={t('Nav:logoAlt')} />
+      <button className={styles.logout} onClick={logout}>
+        {t('Nav:logout')}
+      </button>
+    </nav>
   );
 }
 
